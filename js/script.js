ff3c9a30e050ac30d9a32e87d78e52f8842084f4
Ivan Shmatko <shmatkolearn@gmail.com>
2024-07-18 16:07:19 +0300

    change design

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@
 $(".burger").on("click", function () {
     $("html").toggleClass("menu-open");
 });
+$(".menu__link").on("click", function () {
+    $("html").removeClass("menu-open");
+});
 
 // changes active table
 function showTable(tableId) {
@@ -34,17 +37,20 @@ $(".popup-book__overlay, .booking__close").on("click", function () {
     $("html").toggleClass("popup-open");
 });
 
+//dropdowns in booking form
 $(document).ready(function () {
     $(".dropdown__button").click(function () {
         var $dropdownList = $(this).siblings(".dropdown__list");
         $(".dropdown__list").not($dropdownList).removeClass("dropdown__list--show");
         $dropdownList.toggleClass("dropdown__list--show");
+        $(".dropdown__button").not(this).removeClass("active");
+        $(this).toggleClass("active");
     });
 
     $(".dropdown__item").click(function () {
         var selectedText = $(this).text();
         var $dropdown = $(this).closest(".dropdown");
-        $dropdown.find(".dropdown__button").text(selectedText);
+        $dropdown.find(".dropdown__button").text(selectedText).removeClass("active");
         $dropdown.find(".dropdown__input").val(selectedText);
         $dropdown.find(".dropdown__list").removeClass("dropdown__list--show");
     });
@@ -52,6 +58,26 @@ $(document).ready(function () {
     $(document).click(function (event) {
         if (!$(event.target).closest(".dropdown").length) {
             $(".dropdown__list").removeClass("dropdown__list--show");
+            $(".dropdown__button").removeClass("active");
         }
+    });
+});
+
+//smooth scrolling
+$(document).ready(function () {
+    var headerHeight = $(".header").outerHeight();
+
+    $(".menu__link").on("click", function (event) {
+        event.preventDefault();
+        var targetId = $(this).attr("href");
+        var targetElement = $(targetId);
+        var targetPosition = targetElement.offset().top - headerHeight - 10;
+
+        $("html, body").animate(
+            {
+                scrollTop: targetPosition,
+            },
+            600
+        );
     });
 });
